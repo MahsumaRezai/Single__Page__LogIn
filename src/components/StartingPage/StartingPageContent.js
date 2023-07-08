3630b9453d0c08b3c936085323eb732a7003e264
Masoumeh Rezai <you@example.com>
2023-07-08 07:21:40 +0430

    last commit for porject

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,11 +1,9 @@
-import Footer from './Footer';
 import classes from './StartingPageContent.module.css';
 
 const StartingPageContent = () => {
   return (
     <section className={classes.starting}>
       <h1>Welcome on Board!</h1>
-      <Footer/>
     </section>
   );
 };
